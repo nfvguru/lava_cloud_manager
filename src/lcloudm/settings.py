253fe5dbb8f3374e3c917dc917f535020698c2ab
nfvguru <nfvguru@gmail.com>
2021-05-31 19:44:20 +0530

    linux setups

--- src/lcloudm/settings.py
+++ src/lcloudm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h81_@4*@^@mcsl54(+lgj!w*8l-haoj0ch(&#7-wlysfbcovq7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.82.43.181']
+ALLOWED_HOSTS = ['10.82.43.181','127.0.0.1']
 
 
 # Application definition
