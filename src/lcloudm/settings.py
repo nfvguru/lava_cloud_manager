5d00135b3ee7926c7c3dfcf8fff4f0c6cf0835a7
nfvguru@gmail.com <nfvguru@gmail.com>
2021-05-31 22:22:04 +0530

    DB setup 1

--- src/lcloudm/settings.py
+++ src/lcloudm/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'lcloudm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'cloudm',
+        'USER': 'cloudm',
+        'PASSWORD' : 'cloudm',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
