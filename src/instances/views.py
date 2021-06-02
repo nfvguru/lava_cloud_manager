5fc74cd85e5c7bf752c3c7c19db27c5ca6677186
nfvguru@gmail.com <nfvguru@gmail.com>
2021-06-02 23:14:04 +0530

    RESTful calls

--- src/instances/views.py
+++ src/instances/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 
 def index(request):
-    return HttpResponse("Welcome to Lava Cloud Manager")
+    response = requests.get('http://127.0.0.1:9000/instance')
+    myval = response.json()
+    #return HttpResponse("Welcome to Lava Cloud Manager")
+    return JsonResponse(myval)
