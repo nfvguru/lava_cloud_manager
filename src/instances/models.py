e2886dad56d3edc3dca5b4d362a8191fd0fbecaa
nfvguru@gmail.com <nfvguru@gmail.com>
2021-06-06 15:20:27 +0530

    Tmeplates

--- src/instances/models.py
+++ src/instances/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class InstanceType(models.Model):
+    type = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.summary
--- src/instances/views.py
+++ src/instances/views.py
@@ -4,10 +4,13 @@ import requests
 # Create your views here.
 from django.http import HttpResponse
 from django.http import JsonResponse
+from .models import InstanceType
 
 
 def index(request):
-    response = requests.get('http://127.0.0.1:9000/instance')
-    myval = response.json()
-    #return HttpResponse("Welcome to Lava Cloud Manager")
-    return JsonResponse(myval)
+    # response = requests.get('http://127.0.0.1:9000/instance')
+    # myval = response.json()
+    # return HttpResponse("Welcome to Lava Cloud Manager")
+    # return JsonResponse(myval)
+    inst_types = InstanceType.objects
+    return render(request, 'instances/index.html', {'my_insts':inst_types})
